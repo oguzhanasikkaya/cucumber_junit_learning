50bbfa1a1d15075f53e2cb74ceefa34e85250ef7
oguzhanasikkaya <oguzhanasikkaya@yahoo.com>
2022-09-28 13:15:23 +0300

    lesson 1

--- src/test/java/com/cydeo/runner/CukesRunner.java
+++ src/test/java/com/cydeo/runner/CukesRunner.java
@@ -6,9 +6,11 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+        plugin = "html:target/cucumber-report.html ",
         features = "src/test/resources/features",
         glue = "com/cydeo/step_definitions",
-        dryRun = true
+        dryRun = false,
+        tags = ""
 )
 public class CukesRunner {
 }
--- src/test/java/com/cydeo/step_definitions/Login_StepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/Login_StepDefinitions.java
@@ -20,16 +20,34 @@ public class Login_StepDefinitions {
         //TODO:
         System.out.println("User should see the dashboard");
     }
-
-
-
+    //------------------------------------------------------------
 
     @When("user enters student username")
     public void userEntersStudentUsername() {
+        System.out.println("User enters student username");
+
+
     }
 
     @And("user enters student password")
     public void userEntersStudentPassword() {
+        System.out.println("User enters student password");
     }
+    //------------------------------------------------------------
+
+    @When("user enters admin username")
+    public void userEntersAdminUsername() {
+        System.out.println("User enters admin username");
+    }
+
+    @And("user enters admin password")
+    public void userEntersAdminPassword() {
+        System.out.println("User enters student password");
+    }
+
+
+
+
+
 
 }
