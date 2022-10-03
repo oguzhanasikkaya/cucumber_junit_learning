2514d7192d9551443f4040fe31a6c83c771bf1db
oguzhanasikkaya <oguzhanasikkaya@yahoo.com>
2022-10-03 11:14:04 +0300

    day 15-hooks

--- src/test/java/com/cydeo/runner/CukesRunner.java
+++ src/test/java/com/cydeo/runner/CukesRunner.java
@@ -12,8 +12,8 @@ import org.junit.runner.RunWith;
                 },
         features = "src/test/resources/features",
         glue = "com/cydeo/step_definitions",
-        dryRun = true,
-        tags = "@wip"
+        dryRun = false,
+        tags = "@smoke"
 )
 public class CukesRunner {
 }
